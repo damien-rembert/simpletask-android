4dbe55aab0e5237d5eac863be5a44d2176924d4d
Damien Rembert <damien.rembert@gmail.com>
2025-10-01 22:59:09 +0100

    remove deprecated API usage

--- app/src/main/java/nl/mpcjanssen/simpletask/task/Task.kt
+++ app/src/main/java/nl/mpcjanssen/simpletask/task/Task.kt
@@ -327,7 +327,7 @@ class Task(text: String, defaultPrependedDate: String? = null) {
 
     private fun appendToken(token: TToken, trimWhiteSpace: Boolean = true) {
         while (trimWhiteSpace && tokens.isNotEmpty() && tokens.last() is WhiteSpaceToken) {
-            tokens.removeLast()
+            tokens.removeAt(tokens.lastIndex)
         }
         tokens.add(token)
     }
